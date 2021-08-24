0b3e5daf2745f46c577be22d119bb543b1e6013c
oraant <oraant777@gmail.com>
2021-08-24 21:28:40 +0800

    增加以倒计时为主的解锁方式，即起到拦截效果，又降低了解锁负担。

---
+++ timer.py
@@ -0,0 +1,76 @@
+from tkinter import *
+import pyperclip
+from datetime import datetime as dt
+
+
+# ■■■ 说明 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+
+# 一，修改seconds、content等，将主要内容替换为自己要加密的东西
+# 二，使用命令，将其编译为exe文件：pyinstaller -i icon.ico -Fw timer.py
+# 三，测试生成的程序，放入不同的路径打开，保证没问题，并生成多个不同难度的程序
+# 四，在版本管理中，直接用revert删除刚刚的改动，保证了密文只保存在刚才生成的程序中
+
+
+# ■■■ 配置 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+
+seconds = 3600 # 等待多久后展示内容
+content = 'password' # 等待完毕后，展示给用户的解锁内容
+weeks = [6,7] # 允许使用的星期
+begin = '07:00' # 允许使用的开始时间
+end = '15:00' # 允许使用的结束时间
+
+
+# ■■■ 代码 ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+
+def T(string): return dt.strptime(string,'%H:%M').time()
+
+class Window:
+
+    def __init__(self, seconds, content, weeks, begin, end):
+        self.seconds = seconds
+        self.content = content
+        self.weeks   = weeks
+        self.begin   = begin
+        self.end     = end
+
+        self.root = Tk()
+        self.root.title('python信息代价锁 - 非对称决策阻碍装置 - 时间版')
+        self.root.resizable(False, False)
+        self.root.geometry('300x200')
+        self.root.configure(bg="#fefefe")
+
+        self.btn_text=StringVar(); self.btn_text.set('')
+        self.btn = Button(self.root, textvariable=self.btn_text, borderwidth=2, width=15, height=2, font=('Microsoft Yahei', 13), command=self.copy, state="disabled")
+        self.btn.pack(pady=60)
+
+
+    def copy(self):
+        pyperclip.copy(self.content)
+        self.btn_text.set('复制成功')
+
+
+    def start(self):
+        self.root.after(1, self.check)
+        self.root.mainloop()
+
+    def check(self): # 检查星期和时间
+        print(T(self.begin) , dt.now().time() , T(self.end))
+        print(T(self.begin) < dt.now().time() < T(self.end))
+        if dt.now().isoweekday() not in self.weeks: return self.btn_text.set('只有周末可以使用')
+        if not T(self.begin) <= dt.now().time() <= T(self.end) : return self.btn_text.set('%s-%s 能用' % (self.begin, self.end))
+        self.loop()
+
+    def loop(self): # 倒计时
+        if self.seconds == 0: return self.finish()
+        self.btn_text.set('剩余%d秒' % self.seconds)
+        self.seconds -= 1
+        self.root.after(1000, self.loop)
+
+    def finish(self):
+        self.btn_text.set('点击复制')
+        self.btn.configure(state="normal")
+
+
+Window(seconds, content, weeks, begin, end).start()
